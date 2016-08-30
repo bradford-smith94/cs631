f325b19ba86677ff89eb4a2c47ab95026c3e77a0
Bradford Smith <bsmith8@stevens.edu>
2016-08-30 14:47:44 -0400

    Complete intro script
    Ran the intro script (cs631-intro.sh) and completed all the instructions in it.
    These files are the result of that.

---
+++ intro-bsmith8/hello-cs631.c
@@ -0,0 +1,15 @@
+/* Bradford Smith (bsmith8)
+ * CS 631 Intro Assignment hello-cs631.c
+ * 08/30/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include "stdio.h"
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    return 0;
+}
+
