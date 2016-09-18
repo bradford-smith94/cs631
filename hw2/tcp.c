5a361e5727811f2d81627b5682fe6e7fa03a12a9
Bradford Smith <bsmith8@stevens.edu>
2016-09-18 14:30:53 -0400

    Add code to copy files
    Renamed some variables and added comments explaining what each is for.
    
    Makefile will now define BUF_SIZE as the system defined efficient I/O
    size according to `stat -c "%0"` NOTE: this is not system portable and
    needs the GNU implementation of `stat`.
    
    The `tcp` program will now actually copy files by continually reading
    BUF_SIZE bytes from the source and writing them to the destination.
    
    Still need to work out some test cases that don't pass.

--- hw2/tcp.c
+++ hw2/tcp.c
@@ -1,6 +1,6 @@
 /* Bradford Smith
  * CS 631 HW 2 tcp.c
- * 09/17/2016
+ * 09/18/2016
  */
 
 #include <errno.h>
@@ -12,17 +12,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#ifndef PATH_SEP
 #define PATH_SEP '/'
+#endif
+
+#ifndef BUF_SIZE
+#define BUF_SIZE 2048
+#endif
 
 int main(int argc, char** argv)
 {
-    char* src;
-    char* dest;
-    char* fulldest;
-    int srcfd;
-    int destfd;
-    struct stat* srcstat;
-    struct stat* deststat;
+    char* buf;          //copy buffer
+    char* dest;         //destination arg
+    char* fdest;        //full destination path
+    char* src;          //source arg
+    int dfd;            //destination file descriptor
+    int n;              //num bytes read/write
+    int sfd;            //source file descriptor
+    struct stat* dstat; //destination stat output
+    struct stat* sstat; //source stat output
 
     if (argc < 3) //expecting a [src] and [dest]
     {
@@ -40,7 +48,7 @@ int main(int argc, char** argv)
     src = argv[1];
     dest = argv[2];
 
-    if ((srcstat = (struct stat*)malloc(sizeof(struct stat))) == NULL)
+    if ((sstat = (struct stat*)malloc(sizeof(struct stat))) == NULL)
     {
         fprintf(stderr, "%s: unable to malloc: %s\n",
                 argv[0],
@@ -48,7 +56,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if ((deststat = (struct stat*)malloc(sizeof(struct stat))) == NULL)
+    if ((dstat = (struct stat*)malloc(sizeof(struct stat))) == NULL)
     {
         fprintf(stderr, "%s: unable to malloc: %s\n",
                 argv[0],
@@ -56,7 +64,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if (stat(src, srcstat) < 0)
+    if (stat(src, sstat) < 0)
     {
         fprintf(stderr, "%s: cannot stat '%s': %s\n",
                 argv[0],
@@ -65,7 +73,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if ((srcstat->st_mode & S_IFMT) == S_IFDIR)
+    if ((sstat->st_mode & S_IFMT) == S_IFDIR)
     {
         fprintf(stderr, "%s: '%s' is a directory\n",
                 argv[0],
@@ -74,39 +82,46 @@ int main(int argc, char** argv)
     }
 
     //if dest exists and is a directory
-    if ((stat(dest, deststat) == 0) && ((deststat->st_mode & S_IFMT) == S_IFDIR))
+    if ((stat(dest, dstat) == 0) && ((dstat->st_mode & S_IFMT) == S_IFDIR))
     {
-        fulldest = (char*)malloc(strlen(dest) + strlen(src) + 1 * sizeof(char));
-        if ((sprintf(fulldest, "%s%c%s", dest, PATH_SEP, src)) < 0)
+        fdest = (char*)malloc((strlen(dest) + strlen(src) + 1) * sizeof(char));
+        if (fdest == NULL)
+        {
+            fprintf(stderr, "%s: unable to malloc destination filename: %s\n",
+                    argv[0],
+                    strerror(errno));
+            return 1;
+        }
+
+        if ((sprintf(fdest, "%s%c%s", dest, PATH_SEP, src)) < 0)
         {
             fprintf(stderr, "%s: unable to create destination filename: %s\n",
                     argv[0],
                     strerror(errno));
             return 1;
         }
-
-        if ((destfd = open(fulldest, O_WRONLY|O_CREAT, 00644)) < 0)
-        {
-            fprintf(stderr, "%s: unable to open '%s': %s\n",
-                    argv[0],
-                    fulldest,
-                    strerror(errno));
-            return 1;
-        }
     }
     else
     {
-        if ((destfd = open(dest, O_WRONLY|O_CREAT, 00644)) < 0)
+        if ((fdest = strdup(dest)) == NULL)
         {
-            fprintf(stderr, "%s: unable to open '%s': %s\n",
+            fprintf(stderr, "%s: unable to malloc destination filename: %s\n",
                     argv[0],
-                    dest,
                     strerror(errno));
             return 1;
         }
     }
 
-    if ((srcfd = open(src, O_RDONLY)) < 0)
+    if ((dfd = open(fdest, O_WRONLY|O_CREAT, 00644)) < 0)
+    {
+        fprintf(stderr, "%s: unable to open '%s': %s\n",
+                argv[0],
+                dest,
+                strerror(errno));
+        return 1;
+    }
+
+    if ((sfd = open(src, O_RDONLY)) < 0)
     {
         fprintf(stderr, "%s: unable to open '%s': %s\n",
                 argv[0],
@@ -115,8 +130,39 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    (void)close(srcfd);
-    (void)close(destfd);
+    if ((buf = (char*)malloc((BUF_SIZE + 1) * sizeof(char))) == NULL)
+    {
+        fprintf(stderr, "%s: unable to malloc: %s\n",
+                argv[0],
+                strerror(errno));
+        return 1;
+    }
+
+    while ((n = read(sfd, buf, BUF_SIZE)) > 0)
+    {
+        if ((n = write(dfd, buf, n)) < 0)
+        {
+            fprintf(stderr, "%s: unable to write: %s\n",
+                    argv[0],
+                    strerror(errno));
+            return 1;
+        }
+    }
+    if (n < 0)
+    {
+        fprintf(stderr, "%s: unable to read: %s\n",
+                argv[0],
+                strerror(errno));
+        return 1;
+    }
+
+    (void)free(buf);
+    (void)free(fdest);
+    (void)free(dstat);
+    (void)free(sstat);
+
+    (void)close(dfd);
+    (void)close(sfd);
 
     return 0;
 }
