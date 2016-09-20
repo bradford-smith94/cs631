48d2510946016fd7ad029369862294e21c0a9d19
Bradford Smith <bsmith8@stevens.edu>
2016-09-20 16:06:13 -0400

    Add files for start of Midterm project
    Add man page as specification see: `midterm/man1/ls.1`.
    
    Add script to for testing `ls-test`.
    
    Add header file under include. Add Makefile. Add `ls.c` source, nothing
    in it right now but this will be the minimally needed source files.

---
+++ midterm/include/ls.h
@@ -0,0 +1,11 @@
+/* Bradford Smith (bsmith8)
+ * CS 631 midterm ls.h
+ * 09/20/2016
+ */
+
+#ifndef _LS_H_
+#define _LS_H_
+
+int main(int, char**);
+
+#endif /* _LS_H_ */
---
+++ midterm/ls.c
@@ -0,0 +1,11 @@
+/* Bradford Smith (bsmith8)
+ * CS 631 Midterm ls.c
+ * 09/20/2016
+ */
+
+#include "ls.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
