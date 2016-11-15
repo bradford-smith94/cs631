cdf9440db85779b1c6544edcd018f9b6163b4158
Bradford Smith <bsmith8@stevens.edu>
2016-11-14 23:07:18 -0500

    Add spec and header for libgreet exercise

---
+++ libgreet/include/greeting.h
@@ -0,0 +1,16 @@
+/* Bradford Smith (bsmith8)
+ * CS 631 libgreet Exercise greeting.h
+ * 11/14/2016
+ */
+
+#ifndef _GREETING_H_
+#define _GREETING_H_
+
+char*   getgreeting(void);
+
+int     setgreeting(const char*);
+
+void    greet(void);
+void    hello(const char*, const char*);
+
+#endif /* _GREETING_H_ */
