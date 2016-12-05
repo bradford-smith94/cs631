b69f4b29582a67c4dc1581ab27c1da624db4e475
Bradford Smith <bsmith8@stevens.edu>
2016-12-05 14:30:52 -0500

    Make test output better, and cleanup
    Test script will now output whether second test passed or failed and
    will remove files it creates.

--- hw4/test.sh
+++ hw4/test.sh
@@ -7,5 +7,13 @@ echo ""
 echo "../group3/http.c test:"
 AED_PASS="somethinglongenoughtomakethisworthwile" ./aed -e <../group3/http.c >file.enc
 AED_PASS="somethinglongenoughtomakethisworthwile" ./aed -d <file.enc >file.dec
+DIFF=$(diff ../group3/http.c file.dec)
+if [ "$DIFF" == "" ]; then
+    echo "PASS"
+else
+    echo $DIFF
+fi
 echo ""
 
+rm file.enc
+rm file.dec
