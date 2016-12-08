9f4d857fae5383aca495ec5b755dac1a5fc0de88
Bradford Smith <bsmith8@stevens.edu>
2016-12-07 19:55:08 -0500

    Add Makefile, main and header for sish

---
+++ sish/include/sish.h
@@ -0,0 +1,11 @@
+/* Bradford Smith (bsmith8)
+ * CS 631 sish sish.h
+ * 12/07/2016
+ */
+
+#ifndef _SISH_H_
+#define _SISH_H_
+
+int main(int, char**);
+
+#endif /* _SISH_H_ */
---
+++ sish/main.c
@@ -0,0 +1,11 @@
+/* Bradford Smith (bsmith8)
+ * CS 631 sish main.c
+ * 12/07/2016
+ */
+
+#include "sish.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
