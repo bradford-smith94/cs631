b30a42d3dba50de4ce3b5e8e4a7de6580273f4b3
Bradford Smith <bsmith8@stevens.edu>
2016-12-14 14:02:14 -0500

    Add basic structure and getopt handling

--- sish/include/sish.h
+++ sish/include/sish.h
@@ -1,10 +1,16 @@
 /* Bradford Smith (bsmith8)
  * CS 631 sish sish.h
- * 12/07/2016
+ * 12/14/2016
  */
 
 #ifndef _SISH_H_
 #define _SISH_H_
+
+#define NAME "sish"
+#define PROMPT_STR "sish$ "
+
+unsigned char gl_exit_code;
+char gl_trace;
 
 int main(int, char**);
 
--- sish/main.c
+++ sish/main.c
@@ -1,11 +1,83 @@
 /* Bradford Smith (bsmith8)
  * CS 631 sish main.c
- * 12/07/2016
+ * 12/14/2016
  */
 
 #include "sish.h"
 
+#include <bsd/stdlib.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 int main(int argc, char** argv)
 {
-    return 0;
+    int opt;
+    size_t len;
+    char* command;
+
+    command = NULL;
+    gl_exit_code = EXIT_SUCCESS;
+    gl_trace = 0;
+
+    setprogname(NAME);
+
+    while ((opt = getopt(argc, argv, "c:x")) != -1)
+    {
+        switch (opt)
+        {
+            case 'c':
+                if ((command = strdup(optarg)) == NULL)
+                {
+                    (void)fprintf(stderr, "%s: strdup failure: %s\n",
+                                  getprogname(),
+                                  strerror(errno));
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 'x':
+                gl_trace = 1;
+                break;
+        }
+    }
+    /* TODO add check that no other parameters are given? */
+
+    if (command != NULL)
+    {
+    }
+    else
+    {
+        while (1)
+        {
+            (void)printf(PROMPT_STR);
+            len = 0;
+            errno = 0;
+            if (getline(&command, &len, stdin) < 0)
+            {
+                if (errno)
+                {
+                    (void)fprintf(stderr, "%s: getline failure: %s\n",
+                                  getprogname(),
+                                  strerror(errno));
+                    return EXIT_FAILURE;
+                }
+                else
+                {
+                    (void)fprintf(stderr, "%s: getline: unexpected EOF\n",
+                                  getprogname());
+                    return EXIT_FAILURE;
+                }
+            }
+
+            /* handle command */
+
+            free(command);
+            command = NULL;
+        }
+    }
+
+    return gl_exit_code;
 }
