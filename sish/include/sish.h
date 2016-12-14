0f04529296dc163829d4bbe7f1d3f0ae2a19cd4f
Bradford Smith <bsmith8@stevens.edu>
2016-12-14 17:02:39 -0500

    Setup SHELL environment variable
    Uses the /proc/<pid>/exe symlink to get the full path of the executable
    and sets it as the environment variable 'SHELL'.

--- sish/include/sish.h
+++ sish/include/sish.h
@@ -6,9 +6,18 @@
 #ifndef _SISH_H_
 #define _SISH_H_
 
+#include <sys/types.h> /* for definition of pid_t */
+
+#define CD "cd"
+#define CURRENT_PID_STR "$$"
+#define ECHO "echo"
+#define ENV_SHELL "SHELL"
+#define EXIT "exit"
+#define EXIT_STATUS_STR "$?"
 #define NAME "sish"
 #define PROMPT_STR "sish$ "
 
+pid_t gl_current_pid;
 unsigned char gl_exit_code;
 char gl_trace;
 
--- sish/init.c
+++ sish/init.c
@@ -7,16 +7,24 @@
 
 #include <bsd/stdlib.h>
 
+#include <sys/types.h>
+
 #include <errno.h>
+#include <limits.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /* pre: none
- * post: initialize global variables
+ * post: initialize global variables and environment
  */
 void init()
 {
+    char path[PATH_MAX];
+    char realpath[PATH_MAX];
+
+    gl_current_pid = getpid();
     gl_exit_code = EXIT_SUCCESS;
     gl_trace = 0;
 
@@ -25,6 +33,24 @@ void init()
     if (signal(SIGINT, handleSigInt) == SIG_ERR)
     {
         (void)fprintf(stderr, "%s: signal failure: %s\n",
+                      getprogname(),
+                      strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* find executable path using /proc/<pid>/exe and set $SHELL */
+    (void)sprintf(path, "/proc/%d/exe", gl_current_pid);
+    if (readlink(path, realpath, PATH_MAX) == -1)
+    {
+        (void)fprintf(stderr, "%s: readlink failure: %s\n",
+                      getprogname(),
+                      strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (setenv(ENV_SHELL, realpath, 1) == -1)
+    {
+        (void)fprintf(stderr, "%s: setenv failure: %s\n",
                       getprogname(),
                       strerror(errno));
         exit(EXIT_FAILURE);
